963695928af95efd2cd13efdd94a3528a7356bdd
MunirYounos <eaaamy@gmail.com>
2022-06-07 09:23:49 +0200

    custom js setting json and announcement files modified

--- assets/custom.js
+++ assets/custom.js
@@ -79,71 +79,72 @@ tabItems.forEach(item => {
   item.addEventListener('click', selectItem);
 });
 
+
 // relief tabs
-const homeTabBtns = document.querySelectorAll('.home-tabs__wrapper-btns-btn');
-const homeTabItems = document.querySelectorAll('.home-tabs__wrapper-content-img');
+// const homeTabBtns = document.querySelectorAll('.home-tabs__wrapper-btns-btn');
+// const homeTabItems = document.querySelectorAll('.home-tabs__wrapper-content-img');
 
-function selectHomeTabContents(e) {
-  removeActiveHomeTabClass();
-  removeActiveHomeTabItemClass();
-  this.classList.add('active');
-  const homeTabContentItems = document.querySelector(`#${this.id}-content`);
-  homeTabContentItems.classList.add('active');
-}
+// function selectHomeTabContents(e) {
+//   removeActiveHomeTabClass();
+//   removeActiveHomeTabItemClass();
+//   this.classList.add('active');
+//   const homeTabContentItems = document.querySelector(`#${this.id}-content`);
+//   homeTabContentItems.classList.add('active');
+// }
 
-homeTabBtns.forEach(tabbtn => tabbtn.addEventListener('click', selectHomeTabContents));
+// homeTabBtns.forEach(tabbtn => tabbtn.addEventListener('click', selectHomeTabContents));
 
-const removeActiveHomeTabClass = () => {
-  homeTabBtns.forEach(tabbtn => {
-    tabbtn.classList.remove('active')
-  })
-}
-const removeActiveHomeTabItemClass = () => {
-  homeTabItems.forEach(homeTabItem => {
-    homeTabItem.classList.remove('active')
-  })
-}
-//topbar counter
-let announcementCounterEndText = document.querySelector('.announcement-end-text');
-let announcementEndTextBack = document.querySelector('.announcement-message');
-const announcementDateId = document.getElementById('announcement_counter');
-let announcementEventDate = announcementDateId.getAttribute('data-announcement-date');
-function TopBarUpdateTimer(at, someId ) {
-future = Date.parse(at);
-now = new Date();
-diff = future - now;
+// const removeActiveHomeTabClass = () => {
+//   homeTabBtns.forEach(tabbtn => {
+//     tabbtn.classList.remove('active')
+//   })
+// }
+// const removeActiveHomeTabItemClass = () => {
+//   homeTabItems.forEach(homeTabItem => {
+//     homeTabItem.classList.remove('active')
+//   })
+// }
+// //topbar counter
+// let announcementCounterEndText = document.querySelector('.announcement-end-text');
+// let announcementEndTextBack = document.querySelector('.announcement-message');
+// const announcementDateId = document.getElementById('announcement_counter');
+// let announcementEventDate = announcementDateId.getAttribute('data-announcement-date');
+// function TopBarUpdateTimer(at, someId ) {
+// future = Date.parse(at);
+// now = new Date();
+// diff = future - now;
 
-if(diff >= 0){
-days = Math.floor(diff / (1000 * 60 * 60 * 24));
-hours = Math.floor(diff / (1000 * 60 * 60));
-mins = Math.floor(diff / (1000 * 60));
-secs = Math.floor(diff / 1000);
-	//add zeros
-	function addZero(num) {
-		return ("0" + parseInt(num)).substr(-2);
-	}
-d = addZero(days);
-h = addZero(hours - days * 24);
-m = addZero(mins - hours * 60);
-s = addZero(secs - mins * 60);
+// if(diff >= 0){
+// days = Math.floor(diff / (1000 * 60 * 60 * 24));
+// hours = Math.floor(diff / (1000 * 60 * 60));
+// mins = Math.floor(diff / (1000 * 60));
+// secs = Math.floor(diff / 1000);
+// 	//add zeros
+// 	function addZero(num) {
+// 		return ("0" + parseInt(num)).substr(-2);
+// 	}
+// d = addZero(days);
+// h = addZero(hours - days * 24);
+// m = addZero(mins - hours * 60);
+// s = addZero(secs - mins * 60);
 
-  if(d<1){
-    someId.innerHTML = `
-    <div class="announcement_number"><span class="digit">${h}</span><span class="announcement_text">S<span class="hide-small">tunden</span></span></div>
-    <div class="announcement_number"><span class="digit">${m}</span><span class="announcement_text">M<span class="hide-small">inuten</span></span></div>
-    <div class="announcement_number"><span class="digit">${s}</span><span class="announcement_text">S<span class="hide-small">ekunden</span></span></div>
-    `
-  }else{
-    someId.innerHTML = `
-    <div class="announcement_number"><span class="digit">${d}</span><span class="announcement_text">T<span class="hide-small">age</span></span></div>
-    <div class="announcement_number"><span class="digit">${h}</span><span class="announcement_text">S<span class="hide-small">tunden</span></span></div>
-    <div class="announcement_number"><span class="digit">${m}</span><span class="announcement_text">M<span class="hide-small">inuten</span></span></div>
-    <div class="announcement_number"><span class="digit">${s}</span><span class="announcement_text">S<span class="hide-small">ekunden</span></span></div>
-    `
-  }
-}else {
-	announcementEndTextBack.innerHTML = `${announcementCounterEndText.innerHTML}`;
-	someId.innerHTML ='';
-}
-}
-setInterval('TopBarUpdateTimer(announcementEventDate, announcementDateId  )', 1000);
+//   if(d<1){
+//     someId.innerHTML = `
+//     <div class="announcement_number"><span class="digit">${h}</span><span class="announcement_text">S<span class="hide-small">tunden</span></span></div>
+//     <div class="announcement_number"><span class="digit">${m}</span><span class="announcement_text">M<span class="hide-small">inuten</span></span></div>
+//     <div class="announcement_number"><span class="digit">${s}</span><span class="announcement_text">S<span class="hide-small">ekunden</span></span></div>
+//     `
+//   }else{
+//     someId.innerHTML = `
+//     <div class="announcement_number"><span class="digit">${d}</span><span class="announcement_text">T<span class="hide-small">age</span></span></div>
+//     <div class="announcement_number"><span class="digit">${h}</span><span class="announcement_text">S<span class="hide-small">tunden</span></span></div>
+//     <div class="announcement_number"><span class="digit">${m}</span><span class="announcement_text">M<span class="hide-small">inuten</span></span></div>
+//     <div class="announcement_number"><span class="digit">${s}</span><span class="announcement_text">S<span class="hide-small">ekunden</span></span></div>
+//     `
+//   }
+// }else {
+// 	announcementEndTextBack.innerHTML = `${announcementCounterEndText.innerHTML}`;
+// 	someId.innerHTML ='';
+// }
+// }
+// setInterval('TopBarUpdateTimer(announcementEventDate, announcementDateId  )', 1000);
